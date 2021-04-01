2df8b0000beaf85cca3bba4f0c68e84e2d7fab76
Janos Pasztor <janos@pasztor.at>
2021-04-01 14:02:24 +0200

    Lint fix

--- pool_test.go
+++ pool_test.go
@@ -171,36 +171,28 @@ func TestOneServiceStartupCrash(t *testing.T) {
 
 func TestTwoServices(t *testing.T) {
 	testLock := &sync.Mutex{}
-	pool := service.NewPool(service.NewLifecycleFactory(), log.NewTestLogger(t))
-	poolLifecycle := service.NewLifecycle(pool)
-	poolStarted := make(chan bool)
-	poolStopped := make(chan bool)
-	var poolStates []service.State
-	var serviceStates1 []service.State
-	var serviceStates2 []service.State
-	poolLifecycle.OnRunning(func(s service.Service, l service.Lifecycle) {
-		poolStarted <- true
-	})
-	poolLifecycle.OnStateChange(func(s service.Service, l service.Lifecycle, state service.State) {
-		testLock.Lock()
-		defer testLock.Unlock()
-
-		poolStates = append(poolStates, state)
-	})
+	pool, poolLifecycle, poolStarted, poolStopped, poolStates, serviceStates1, serviceStates2 := setupPoolForTwoServiceTest(
+		t,
+		testLock,
+	)
 
 	s1 := newTestService("Test service 1")
-	pool.Add(s1).OnStateChange(func(s service.Service, l service.Lifecycle, state service.State) {
-		testLock.Lock()
-		defer testLock.Unlock()
-		serviceStates1 = append(serviceStates1, state)
-	})
+	pool.Add(s1).OnStateChange(
+		func(s service.Service, l service.Lifecycle, state service.State) {
+			testLock.Lock()
+			defer testLock.Unlock()
+			serviceStates1 = append(serviceStates1, state)
+		},
+	)
 
 	s2 := newTestService("Test service 2")
-	pool.Add(s2).OnStateChange(func(s service.Service, l service.Lifecycle, state service.State) {
-		testLock.Lock()
-		defer testLock.Unlock()
-		serviceStates2 = append(serviceStates2, state)
-	})
+	pool.Add(s2).OnStateChange(
+		func(s service.Service, l service.Lifecycle, state service.State) {
+			testLock.Lock()
+			defer testLock.Unlock()
+			serviceStates2 = append(serviceStates2, state)
+		},
+	)
 
 	go func() {
 		err := poolLifecycle.Run()
@@ -212,27 +204,75 @@ func TestTwoServices(t *testing.T) {
 
 	<-poolStarted
 	poolLifecycle.Stop(context.Background())
+
+	verifyTwoServiceExecutionOrder(t, testLock, serviceStates1, serviceStates2, poolStates)
+}
+
+func verifyTwoServiceExecutionOrder(
+	t *testing.T,
+	testLock *sync.Mutex,
+	serviceStates1 []service.State,
+	serviceStates2 []service.State,
+	poolStates []service.State,
+) {
 	testLock.Lock()
 	defer testLock.Unlock()
+	assert.Equal(
+		t, []service.State{
+			service.StateStarting,
+			service.StateRunning,
+			service.StateStopping,
+			service.StateStopped,
+		}, serviceStates1,
+	)
+	assert.Equal(
+		t, []service.State{
+			service.StateStarting,
+			service.StateRunning,
+			service.StateStopping,
+			service.StateStopped,
+		}, serviceStates2,
+	)
+	assert.Equal(
+		t, []service.State{
+			service.StateStarting,
+			service.StateRunning,
+			service.StateStopping,
+			service.StateStopped,
+		}, poolStates,
+	)
+}
 
-	assert.Equal(t, []service.State{
-		service.StateStarting,
-		service.StateRunning,
-		service.StateStopping,
-		service.StateStopped,
-	}, serviceStates1)
-	assert.Equal(t, []service.State{
-		service.StateStarting,
-		service.StateRunning,
-		service.StateStopping,
-		service.StateStopped,
-	}, serviceStates2)
-	assert.Equal(t, []service.State{
-		service.StateStarting,
-		service.StateRunning,
-		service.StateStopping,
-		service.StateStopped,
-	}, poolStates)
+func setupPoolForTwoServiceTest(t *testing.T, testLock *sync.Mutex) (
+	service.Pool,
+	service.Lifecycle,
+	chan bool,
+	chan bool,
+	[]service.State,
+	[]service.State,
+	[]service.State,
+) {
+	pool := service.NewPool(service.NewLifecycleFactory(), log.NewTestLogger(t))
+	poolLifecycle := service.NewLifecycle(pool)
+	poolStarted := make(chan bool)
+	poolStopped := make(chan bool)
+	var poolStates []service.State
+	var serviceStates1 []service.State
+	var serviceStates2 []service.State
+	poolLifecycle.OnRunning(
+		func(s service.Service, l service.Lifecycle) {
+			poolStarted <- true
+		},
+	)
+	poolLifecycle.OnStateChange(
+		func(s service.Service, l service.Lifecycle, state service.State) {
+			testLock.Lock()
+			defer testLock.Unlock()
+
+			poolStates = append(poolStates, state)
+		},
+	)
+	return pool, poolLifecycle, poolStarted, poolStopped, poolStates, serviceStates1, serviceStates2
 }
 
 func TestTwoServicesOneCrashed(t *testing.T) {
